11c1271ac7737d842b95a122ea3e8cd7ba4e8896
Stoik <sau.nguyen@outlook.com>
2019-03-14 12:08:01 +0700

    Update directory tree
    Hide version metadata inside scripts dir

--- wikidump/scripts/download-torrent.sh
+++ wikidump/scripts/download-torrent.sh
@@ -2,6 +2,6 @@
 
 mkdir -p data
 
-read -r HASH < "$(dirname $0)/../TORRENT_HASH"
+read -r HASH < "$(dirname $0)/TORRENT_HASH"
 
 aria2c -T "data/${HASH}.torrent" -d data --file-allocation=falloc -V
--- wikidump/scripts/download.sh
+++ wikidump/scripts/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -r VERSION < "$(dirname $0)/../DUMP_VERSION"
+read -r VERSION < "$(dirname $0)/DUMP_VERSION"
 LINK="https://dumps.wikimedia.org/enwiki/${VERSION}/enwiki-${VERSION}-pages-articles.xml.bz2"
 
 cmd="wget -c -np -A 7z ${LINK} -O enwiki-${VERSION}-pages-articles.xml.bz2"
