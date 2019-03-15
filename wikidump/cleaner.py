3fe7b748c024ca34fc3dc36b73ac1f8caea3d715
Stoik <sau.nguyen@outlook.com>
2019-03-15 17:26:50 +0700

    Protect wikicode replacement actions

--- wikidump/cleaner.py
+++ wikidump/cleaner.py
@@ -7,6 +7,13 @@ def remove_file_link(wikicode):
     for link in wikicode.ifilter_wikilinks():
         if 'File:' not in link.title:
             continue
+
+        # If link content not present in wikicode anymore (due to
+        # previous duplicated replacement), a ValueError will be
+        # raised. We should only replace if this link content only
+        # if it is presented in this context.
+        if link not in wikicode:
+            continue
         wikicode.replace(link, '')
 
 
@@ -14,7 +21,11 @@ def normalize_inter_lang_text(wikicode):
     for t in wikicode.ifilter_templates():
         if t.name != 'lang':
             continue
-        _, text = t.params[0:2]
+        if t not in wikicode:
+            continue
+
+        # We only care about the second required param here
+        text = t.params[1]
         text_value = text.value.strip_code()
         wikicode.replace(t, text_value)
 
@@ -23,7 +34,15 @@ def clean_up_ref_tag(wikicode):
     for tag in wikicode.ifilter_tags():
         if tag.tag != 'ref':
             continue
-        wikicode.replace(tag, '')
+
+        # There is a weird situation that the Tag object presents in
+        # wikicode, but deep_search method could not find it.
+        # We must failback to string representation of tag object to
+        # replace it in wikicode.
+        if wikicode.contains(tag):
+            wikicode.replace(tag, '')
+        elif wikicode.contains(str(tag)):
+            wikicode.replace(str(tag), '')
 
 
 def strip(src):
